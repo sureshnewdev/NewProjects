615491dad7c039bb31894a2096ae27c49da6316d
Murali-MK <muralimk@outlook.com>
2022-02-18 11:16:38 +0530

    Add comments

--- ConsoleApp12/Program.cs
+++ ConsoleApp12/Program.cs
@@ -7,6 +7,8 @@ namespace ConsoleApp12
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            Console.WriteLine( "Welcome all");
         }
     }
 }
